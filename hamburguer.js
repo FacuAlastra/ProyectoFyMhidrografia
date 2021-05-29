ff06c28002ae4ca586b8d2b94c996474eb6207ed
Facu Alastra <facucnsa@gmail.com>
2021-05-29 20:19:49 -0300

    hice el menú hamburguesa con js

---
+++ hamburguer.js
@@ -0,0 +1,9 @@
+
+menu.addEventListener("click",()=>{
+
+    // si se cumple el evento entonces:
+        console.log("Hiciste click!");
+        // toggle: prende y apaga. Se fija si ya está aplicada y si es asi la elimina, sino la agrega
+        menujs.classList.toggle('desplegar');
+        
+    });
